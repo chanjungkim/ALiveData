48b78216f691872e83e00019b6f96ea362dea58d
evan <chanjungskim@gmail.com>
2021-06-18 02:47:58 +0900

    Fix: bug

--- sample/src/main/java/org/koreanlab/sample/MainActivity.kt
+++ sample/src/main/java/org/koreanlab/sample/MainActivity.kt
@@ -10,22 +10,35 @@ import org.koreanlab.alivedata.ALiveArrayListData
 class MainActivity : AppCompatActivity() {
     val aliveArrayListData = ALiveArrayListData<String>()
 
-    val txtList = arrayListOf<String>("말", "고양이", "늑대", "염소")
+    val txtList = arrayListOf<String>("horse", "cat", "wolf", "goat")
     var tvPrint: TextView? = null
 
     var cnt = 0
     val handler = Handler()
     var runnable = Runnable {
+        // This is valid for the first time
         aliveArrayListData
             .add("\nADD 1:${txtList[1]}")
             .add("\nADD 2:${txtList[2]}")
             .update()
 
+        // This won't do anything
         aliveArrayListData
             .add("\nADD 3:${txtList[1]}")
             .add("\nADD 4:${txtList[2]}")
 
-        runTest()
+        // You need to set new value to make aliveArrayListData usable
+        aliveArrayListData
+            .set(txtList)
+            .add("\n===========")
+            .update()
+
+        // You need to use keep() for keeping the current data of ALiveArrayListData after calling update.
+        aliveArrayListData
+            .keep()
+            .add("\nADD 5:${txtList[1]}")
+            .add("\nADD 6:${txtList[2]}")
+            .update()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         // Observe
         aliveArrayListData.observe(this, Observer {
             tvPrint!!.text = "ALiveData Test ${++cnt}\n ${it.toString()}"
+            runTest()
         })
 
         // init data
